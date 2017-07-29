69b50cb7244558eaa03ca8ed916611e443a69230
Mike Pinto <mikepinto.msp@gmail.com>
2017-07-29 01:16:20 +0100

    dynamic char menu loading

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,22 @@
 
 var importchars = function importchars(){
 	d3.json("./assets/data/map_chars.json", function(err, data) {
-		console.log(data);
+
+		for(let i=0; i<data.length; i++){
+			/*
+			<tr>
+				<td class="char-card">
+					<img src="./assets/chars/kazumi_thumbnail.png">
+					<p>Kazumi</p>
+					<div class="frame"></div>
+				</td>
+			</tr>
+			*/
+			var tname = data[i].c.split(" ");
+			if(data[i].i == "11")
+				tname = data[i].c.split("-");
+			d3.select(".inner-table table").append("tr")
+				.html("<td class=\"char-card\"><img src=\"./assets/chars/"+tname.join("").toLowerCase()+"_thumbnail.png\"><p>"+data[i].c+"</p><div class=\"frame\"></div></td>");
+		}
 	});
 }
